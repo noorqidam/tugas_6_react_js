37abdee215cb73e2437c86ef288136a1f05195c1
noorqidam <noorqidam@gmail.com>
2019-09-14 21:29:55 +0700

    Tugas 6 React JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import MenuHome from "../src/Page/MenuHome";
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <MenuHome />
+        <br />
+      </div>
+    )
+  }
+}
+
+export default App;
